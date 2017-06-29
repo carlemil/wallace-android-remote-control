bdafc32701378560063b779b7ad354908b04a167
Carl-Emil Kjellstrand <erbsman@gmail.com>
2017-06-29 11:04:43 +0200

    opps.

---
+++ app/src/main/java/se/jayway/wallaceremotecontrol/MyFirebaseInstanceIdService.java
@@ -0,0 +1,10 @@
+package se.jayway.wallaceremotecontrol;
+
+import FirebaseInstanceIdService;
+
+/**
+ * Created by carlemil on 6/29/17.
+ */
+
+public class MyFirebaseInstanceIdService extends FirebaseInstanceIdService {
+}
---
+++ app/src/main/java/se/jayway/wallaceremotecontrol/MyFirebaseMessagingService.java
@@ -0,0 +1,10 @@
+package se.jayway.wallaceremotecontrol;
+
+import FirebaseMessagingService;
+
+/**
+ * Created by carlemil on 6/29/17.
+ */
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+}
