12f4ef203896a897e0c21eee5145a48c8b418132
Carl-Emil Kjellstrand <erbsman@gmail.com>
2017-06-29 11:46:10 +0200

    added seekbars.

--- app/src/main/java/se/jayway/wallaceremotecontrol/MainActivity.kt
+++ app/src/main/java/se/jayway/wallaceremotecontrol/MainActivity.kt
@@ -2,6 +2,7 @@ package se.jayway.wallaceremotecontrol
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,8 +10,18 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-//        robotController.
+        seekBarLeft.setOnTouchListener{v, e ->
+            writeToSocket(seekBarLeft.progress-100, seekBarRight.progress-100)
+            false
+        }
+        seekBarRight.setOnTouchListener{v, e ->
+            writeToSocket(seekBarLeft.progress-100, seekBarRight.progress-100)
+            false
+        }
 
+    }
 
+    private fun writeToSocket(rightMotor: Int, leftMotor: Int) {
+        
     }
 }
